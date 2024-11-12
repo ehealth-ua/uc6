36502740f41fc475cccdc7d44d4ccbdffdb1685f
5HT <maxim@synrc.com>
2024-11-12 15:26:14 +0200

    cleanup warnings

--- apps/uc6/lib/uc6.ex
+++ apps/uc6/lib/uc6.ex
@@ -4,6 +4,6 @@ defmodule EHCS.UC6 do
 
   def get_prescription(prescription_id) when is_binary(prescription_id) do Prescriptions.get_prescription(prescription_id) end
   def search(tax_id \\ nil, limit \\ 100, offset \\ 0) do Prescriptions.search(tax_id, limit, offset) end
-  def send_prescription(id, prescription_id, tax_id, patient_name) do :ok end
+  def send_prescription(_id, _prescription_id, _tax_id, _patient_name) do :diia_trembita_channel end
 
 end
